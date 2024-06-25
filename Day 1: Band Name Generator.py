90303b7fe2634bb129d8ec6ff98f75de0aefdaa6
arthiom <52601459+arthiom@users.noreply.github.com>
2024-06-25 21:54:20 +0300

    Delete Day 1: Band Name Generator.py

--- Day 1: Band Name Generator.py
+++
@@ -1,5 +0,0 @@
-# Band Name Generator#
-print("Welcom to Band Name Generator.") 
-colour = input("What is your favorite colour?\n")
-position = input("What is your favorite sex position?\n")
-print("You should make your band name: " + colour + " " + position)
